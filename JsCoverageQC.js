be09d45066e1efbf1e1d2e9f4ecc73ed4474df14
Remi Marenco <remi.marenco@gmail.com>
2015-03-11 18:03:13 +0100

    Added style.css and 'upload' on client side of VCF / Exon / Amplicon / DoNotCallFile

--- JsCoverageQC.js
+++ JsCoverageQC.js
@@ -1,7 +1,63 @@
-/* TODO: 
+/* TODO:
 	- Récupérer des fichiers en js client only
 	- Faire le découpage en classe comme coverageQC + Faire algo main Java -> Javascript
 	- Etablir une sortie pour vérifier que les données sont ok comme Java
 	- Faire le html qui correspond
 	- Utiliser les libs identiques pour faire le boulot
  */
+
+window.onload = function(){
+
+	function getFile(htmlIdName){
+		var fileInput = document.getElementById(htmlIdName);
+		//var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+		var reader = new FileReader();
+
+		fileInput.addEventListener('change', function(e){
+			var file = fileInput.files[0];
+
+			// When the file is loaded
+			reader.onload = function(e){
+				//fileDisplayArea.innerText = reader.result;
+				alert("File loaded!");
+			};
+
+			reader.readAsText(file);
+		});
+
+		return reader;
+	}
+
+	function getParameters(){
+		var readerVcfFile = getFile("vcfFile");
+		var readerExonFile = getFile("exonFile");
+		var readerAmpliconFile = getFile("ampliconFile");
+		var readerDoNotCallFile = getFile("doNotCallFile");
+
+		var parameters = [];
+
+		parameters.vcf = readerVcfFile;
+		parameters.exon = readerExonFile;
+		parameters.amplicon = readerAmpliconFile;
+		parameters.doNotCallFile = readerDoNotCallFile;
+		return parameters;
+	}
+
+	// We can set a boolean to true when ok for processing files
+	var parameters = getParameters();
+
+	var ProcessInput = document.getElementById("Process");
+	ProcessInput.addEventListener('click', function(e){
+			// To process, we first need to check we have all the files
+			if((parameters.vcf && parameters.exon && parameters.amplicon) &&
+			 (parameters.vcf.readyState === 2 && parameters.exon.readyState === 2 && parameters.amplicon.readyState === 2))
+			{
+				console.log("C'est good!");
+			}
+			else
+			{
+				alert("One of the necessaries files (VCF, Exon ou Aplicon) are not yet loaded. Please load them first before process.");
+			}
+		});
+};
