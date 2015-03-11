9ad8a7730bd00398051f06669dc61176c5e8c1a7
Remi Marenco <remi.marenco@gmail.com>
2015-03-11 13:23:44 +0100

    Init of the repo

---
+++ JsCoverageQC.js
@@ -0,0 +1,7 @@
+/* TODO: 
+	- Récupérer des fichiers en js client only
+	- Faire le découpage en classe comme coverageQC + Faire algo main Java -> Javascript
+	- Etablir une sortie pour vérifier que les données sont ok comme Java
+	- Faire le html qui correspond
+	- Utiliser les libs identiques pour faire le boulot
+ */
