a0a15f41c204d4bb69cdbdd0ccf5488c6a8e91ea
Remi Marenco <remi.marenco@gmail.com>
2015-04-16 10:49:16 +0200

    Added the input files and small css modification for the submit button

--- src/scripts/components/JsCoverageQcApp.js
+++ src/scripts/components/JsCoverageQcApp.js
@@ -9,6 +9,40 @@ require('../../styles/main.css');
 
 var imageURL = require('../../images/yeoman.png');
 
+var InputFilesForm = React.createClass({
+	render: function(){
+		return (
+			<div id="page-wrapper">
+				<h1>JsCoverageQC Report</h1>
+				<div>
+					Select a vcf file:
+					<input type="file" id="vcfFile"/>
+				</div>
+				<div>
+					Select an exon bed file
+					<input type="file" id="exonFile"/>
+				</div>
+				<div>
+					Select an amplicon bed file
+					<input type="file" id="ampliconFile"/>
+				</div>
+				<div>
+					Select a DoNotCallFile (optional)
+					<input type="file" id="doNotCallFile"/>
+				</div>
+				<div>
+					Select a TSV variant file (optional)
+					<input type="file" id="variantTsv"/>
+				</div>
+				<div>
+					<input type="submit" id="Process" value="Process"/>
+				</div>
+				<pre id="fileDisplayArea"></pre>
+			</div>
+		);
+	}
+});
+
 var JsCoverageQcApp = React.createClass({
   render: function() {
     return (
@@ -16,6 +50,7 @@ var JsCoverageQcApp = React.createClass({
         <ReactTransitionGroup transitionName="fade">
           <img src={imageURL} />
         </ReactTransitionGroup>
+        <InputFilesForm/>
       </div>
     );
   }
