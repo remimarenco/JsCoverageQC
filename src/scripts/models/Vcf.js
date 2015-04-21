100caaff3b9a559d5c0aa79bc41b2ac01f93b499
Remi Marenco <remi.marenco@gmail.com>
2015-04-21 15:06:13 +0200

    Added some comments in Vcf

--- src/scripts/models/Vcf.js
+++ src/scripts/models/Vcf.js
@@ -4,6 +4,9 @@ var cSortedSet = require("collections/sorted-set");
 var cList = require("collections/list");
 var cSortedMap = require("collections/sorted-map");
 
+/**
+ * [Vcf description]
+ */
 function Vcf(){
 	this.fileName = '';
 	this.doNotCallFile = '';
@@ -12,14 +15,24 @@ function Vcf(){
 	this.variantTsvFileLineCount = 0;
 	this.runDate = new Date();
 	this.geneExons = new cSortedSet(); // TreeSet<GeneExon>
+	/** @type {cList} These are used to construct an IGV link */
 	this.bedBamVcfFileUrls = new cList();
+	/** @type {cSortedMap} key is chr|pos (e.g., "chr9|320001") */
 	this.bases = new cSortedMap(); // TreeMap<String, Base>
 }
 
 Vcf.prototype = {
+	/**
+	 * [getBaseCount description]
+	 * @return {Integer} The number of bases
+	 */
 	getBaseCount: function(){
 		return this.bases.length;
 	},
+	/**
+	 * [getFilteredAnnotatedVariantCount description]
+	 * @return {Integer} [description]
+	 */
 	getFilteredAnnotatedVariantCount: function(){
 		var filteredAnnotatedVariantCount = 0;
 		this.geneExons.forEach(function(geneExon){
