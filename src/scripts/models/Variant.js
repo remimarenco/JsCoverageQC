45783b1064c6462b0ea64133ccc308ffe09c07b4
Remi Marenco <remi.marenco@gmail.com>
2015-04-21 17:33:27 +0200

    Added the static function checkIfOnDoNOTCallList

--- src/scripts/models/Variant.js
+++ src/scripts/models/Variant.js
@@ -31,8 +31,6 @@ function Variant(tsvHeadingLine, tsvDataLine, doNotCalls){
 	this.onTheDoNotCallList = false;
 	this.typeOfDoNotCall = '';
 	this.transcript = '';
-
-
 }
 
 /**
@@ -42,6 +40,54 @@ function Variant(tsvHeadingLine, tsvDataLine, doNotCalls){
  * @return {Variant}            [description]
  */
 Variant.checkIfOnDoNOTCallList = function(variant2, doNotCalls){
+	variant2.onTheDoNotCallList = false;
+	variant2.typeOfDoNotCall = "Not on lab list /Potentially Valid";
+	//TODO: Fix the "Definatively" error
+	var currentltyCanDefinativelyCompare;
+
+	for(var i=0; i < doNotCalls.length; i++){
+		var currentDoNotCall = doNotCalls[i];
+		var doNotCallComparison = null;
+		var variantComparison = null;
+		var doNotCallComparison_transcript = currentDoNotCall.transcript;
+		var variantComparison_transcript = variant2.transcript;
+		var doNotCallComparison_coordinate = currentDoNotCall.coordinate;
+		var variantComparison_coordinate = variant2.coordinate;
+
+		//Currently only checking Transcript_27 and if empty in tsv or do not call will crash
+		if(currentDoNotCall.hgvsc !== null && variant2.hgvscComplete !== null){
+			currentltyCanDefinativelyCompare = true;
+			doNotCallComparison = currentDoNotCall.hgvsc;
+			variantComparison = variant2.hgvscComplete;
+		}
+		else{
+			currentltyCanDefinativelyCompare = false;
+		}
+
+		if(doNotCallComparison_transcript.equals(variantComparison_transcript) &&
+			variantComparison_coordinate.equals(doNotCallComparison_coordinate))
+		{
+			variant2.onTheDoNotCallList = true;
+			//adding a fourth call type, meaning if it is the exact location of a do not call
+			//but does not match by hgvsccomple then a separate warning
+			if(currentltyCanDefinativelyCompare)
+			{
+				if(doNotCallComparison.equals(variantComparison)){
+					variant2.typeOfDoNotCall = currentDoNotCall.calltType;
+					break;
+				}
+				else{
+					variant2.typeOfDoNotCall = "In same location as do not call variant.  However mutation is different";
+					//still look because maybe better matching variant is available, hence don't break loop
+				}
+			}
+			else{
+				variant2.typeOfDoNotCall="In same location as do not call variant. However can't compare if same mutation.";
+				//still look because maybe better matching variant is available, hence don't break loop
+			}
+		}
+	}
+
 	return variant2;
 };
 
