a20bfab8511cf82797c0cc17e253544d63df0fb7
Remi Marenco <remi.marenco@gmail.com>
2015-04-22 18:00:40 +0200

    First BDD tests passed with success on the Amplicon model prototype

--- test/spec/models/Amplicon.js
+++ test/spec/models/Amplicon.js
@@ -1,6 +1,7 @@
 'use strict';
 
 describe('Amplicon', function () {
+  /*
   var React = require('react/addons');
   var JsCoverageQcApp, component;
 
@@ -16,6 +17,25 @@ describe('Amplicon', function () {
   it('should create a new instance of JsCoverageQcApp', function () {
     expect(component).toBeDefined();
   });
+  */
 
-  var ampliconModel = require('models/Amplicon.js');
+  var Amplicon = require('models/Amplicon.js');
+
+  it('should create an instance with a string parameter', function(){
+    var amplicon = new Amplicon("");
+    expect(amplicon).toEqual(jasmine.any(Amplicon));
+  });
+
+  // Test the existence of the compareTo function
+  it('should have a compareTo function', function(){
+    var amplicon = new Amplicon("");
+    expect(amplicon.compareTo).toBeDefined();
+  });
+
+  // Test the construction of the Amplicon with a given file
+  it('', function(){
+    return true;
+  });
+
+  // Test the comparison of the Amplicon with an other amplicon
 });
