d4fe8881005d6a4085fa555fd46673ad382c3930
Remi Marenco <remi.marenco@gmail.com>
2015-04-27 17:04:36 +0200

    Changed from XMLHttpRequest async to sync to get files

---
+++ test/helpers/utilities.js
@@ -0,0 +1,22 @@
+/*function upload_and_checkFireDone(done, object_XMLHttpRequest_And_ValueToStore_forUpload){
+	// Check first if done has been emitted
+	var allReady = true;
+	object_XMLHttpRequest_And_ValueToStore_forUpload.othersXMLHttpRequest.forEach(function(otherXMLHttpRequest){
+		if()
+	});
+	if(reqTsv_readyState === 4 && (reqTsv_status === 200 || reqTsv_status === 0)  && !done_emitted){
+		done();
+	}
+}
+
+
+var reqTsv = new XMLHttpRequest();
+reqTsv.open("GET", tsvFile, true);
+reqTsv.responseType = "text";
+reqTsv.jsCoverageQC.valueToStore
+upload_and_checkFireDone(done, {
+	XMLHttpRequest,
+	valueToStore,
+	othersXMLHttpRequest,
+	done_emitted
+});*/
--- test/spec/models/Vcf.js
+++ test/spec/models/Vcf.js
@@ -4,17 +4,70 @@ var cSortedSet = require("collections/sorted-set");
 var cList = require("collections/list");
 var cSortedMap = require("collections/sorted-map");
 
+function getResponseText(fromUrl){
+	var req = new XMLHttpRequest();
+	req.open("GET", fromUrl, false);
+	req.responseType = "text";
+	req.send();
+	return req.responseText;
+}
+
 describe('Vcf', function(){
 	var Vcf;
-	var vcf;
+	var vcfFileUrl;
+	var vcfText;
+
+	var exonBedFileUrl;
+	var exonBedText;
+
+	var ampliconBedFileName;
+	var ampliconBedText;
+
+	var variantFileUrl;
+	var variantText;
+	var variantFileLineCount;
 
 	beforeEach(function(){
 		Vcf = require('models/Vcf');
 
-		vcf = new Vcf();
+		vcfFileUrl = "http://localhost:8080/base/test/data/sample.genome.vcf";
+		vcfText = getResponseText(vcfFileUrl);
+		exonBedFileUrl = "http://localhost:8080/base/test/data/cancer_panel_26.20140719.exons.bed";
+		exonBedText = getResponseText(exonBedFileUrl);
+		ampliconBedFileName = "cancer_panel_26.20140717.amplicons.bed";
+		ampliconBedText = getResponseText("http://localhost:8080/base/test/data/"+ampliconBedFileName);
+		variantFileUrl = "http://localhost:8080/base/test/data/sample.variant.tsv";
+		variantText = getResponseText(variantFileUrl);
+		variantFileLineCount = variantText.split("\n").length;
 	});
 
 	it('should construct properly with no parameters passed', function(){
+		var vcf = new Vcf();
+
+		expect(vcf.fileUrl).toBeUndefined();
+		expect(vcf.doNotCallFileUrl).toEqual("NO DO NOT CALL FILE USED!");
+		expect(vcf.exonBedFileUrl).toBeUndefined();
+		expect(vcf.ampliconBedFileName).toBeUndefined();
+		expect(vcf.variantTsvFileUrl).toBeUndefined();
+		expect(vcf.variantTsvFileLineCount).toBeUndefined();
+		expect(vcf.runDate).toEqual(jasmine.any(Date));
+		expect(vcf.geneExons).toEqual(jasmine.any(cSortedSet));
+		expect(vcf.bedBamVcfFileUrls).toEqual(jasmine.any(cList));
+		expect(vcf.bases).toEqual(jasmine.any(cSortedMap));
+	});
+
+	it('should construct properly with good parameters passed (no doNotCall)', function(){
+		// TODO: Create a test file uploading function in utilities.js to manage properly the async used by Jasmine
+		// fileUrl => sample.genome.vcf
+		// doNotCallFile => Not yet
+		// exonBedFileUrl => cancer_panel_26.20140719.exons.bed
+		// ampliconBedFileName => cancer_panel_26.20140717.amplicons.bed
+		// variantTsvFileUrl => sample.variant.tsv
+		// variantTsvFileLineCount => Process it (9)
+		// vcf.geneExons.length => 85
+		var vcf = new Vcf(vcfFileUrl, exonBedFileUrl, exonBedText, ampliconBedFileName,
+	variantFileUrl, variantFileLineCount);
+
 		expect(vcf.fileUrl).toBeUndefined();
 		expect(vcf.doNotCallFileUrl).toEqual("NO DO NOT CALL FILE USED!");
 		expect(vcf.exonBedFileUrl).toBeUndefined();
