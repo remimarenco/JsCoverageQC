74dc9e195f254e2c030cb77bce0725498a81ff43
Remi Marenco <remi.marenco@gmail.com>
2015-04-27 18:51:23 +0200

    Added most of the test cases in Vcf jasmine tests

--- test/spec/models/Vcf.js
+++ test/spec/models/Vcf.js
@@ -78,13 +78,120 @@ describe('Vcf', function(){
 		expect(vcf.bedBamVcfFileUrls.length).toEqual(1);
 		expect(vcf.bedBamVcfFileUrls.has(ampliconBedFileName)).toBeTruthy();
 		expect(vcf.bases.length).toEqual(0);
-
-		//TODO: Missing the test of the values
 	});
 
 	describe('and its function', function(){
-		describe('getBaseCount', function(){
+		var vcf;
+		beforeEach(function(){
+				vcf = new Vcf(vcfFileUrl, exonBedFileUrl, exonBedText, ampliconBedFileName,
+			variantFileUrl, variantFileLineCount);
+		});
 
+		describe('getBaseCount', function(){
+			it('should be defined', function(){
+				expect(vcf.getBaseCount).toBeDefined();
+			});
+
+			it('should return 0 when no bases in Vcf object', function(){
+				expect(vcf.getBaseCount()).toEqual(0);
+			});
+		});
+
+		describe('getFilteredAnnotatedVariantCount', function(){
+			it('should be defined', function(){
+				expect(vcf.getFilteredAnnotatedVariantCount).toBeDefined();
+			});
+
+			it('should return 0 when no geneExons in Vcf object', function(){
+				expect(vcf.getFilteredAnnotatedVariantCount()).toEqual(0);
+			});
+
+			/* TODO: Add GeneExons
+			it('should not return 0 when geneExons in Vcf object', function(){
+				expect(vcf.getFilteredAnnotatedVariantCount()).not.toEqual(0);
+			});
+			*/
+		});
+
+		describe('getAmpliconCount', function(){
+			it('should be defined', function(){
+				expect(vcf.getAmpliconCount).toBeDefined();
+			});
+
+			it('should return 0 when no geneExons in Vcf object', function(){
+				expect(vcf.getFilteredAnnotatedVariantCount()).toEqual(0);
+			});
+
+			/* TODO: Add GeneExons
+			it('should not return 0 when geneExons in Vcf object', function(){
+				expect(vcf.getAmpliconCount()).not.toEqual(0);
+			});
+			*/
+		});
+
+		describe('getReadDepthCount', function(){
+			it('should be defined', function(){
+				expect(vcf.getReadDepthCount).toBeDefined();
+			});
+
+			it('should return 0 when no bases in Vcf object', function(){
+				expect(vcf.getReadDepthCount()).toEqual(0);
+			});
+
+			/* TODO: Add GeneExons
+			it('should not return 0 when bases in Vcf object', function(){
+				expect(vcf.getReadDepthCount()).not.toEqual(0);
+			});
+			*/
+		});
+
+		describe('findGeneExonsForChrPos', function(){
+			it('should be defined', function(){
+				expect(vcf.findGeneExonsForChrPos).toBeDefined();
+			});
+
+			it('should return a SortedSet with a 0 length when no geneExons in Vcf object', function(){
+				expect(vcf.findGeneExonsForChrPos()).toEqual(jasmine.any(cSortedSet));
+				expect(vcf.findGeneExonsForChrPos().length).toEqual(0);
+			});
+
+			/* TODO: Add GeneExons
+			it('should return a SortedSet with not a 0 length when no geneExons in Vcf object', function(){
+				expect(vcf.findGeneExonsForChrPos().length).not.toEqual(0);
+			});
+			*/
+		});
+
+		describe('findGeneExonsForChrRange', function(){
+			it('should be defined', function(){
+				expect(vcf.findGeneExonsForChrRange).toBeDefined();
+			});
+
+			it('should return a SortedSet with a 0 length when no geneExons in Vcf object, and no parameters', function(){
+				expect(vcf.findGeneExonsForChrRange()).toEqual(jasmine.any(cSortedSet));
+				expect(vcf.findGeneExonsForChrRange().length).toEqual(0);
+			});
+
+			it('should return a SortedSet with a 0 length when no geneExons in Vcf object, and good parameters', function(){
+				expect(vcf.findGeneExonsForChrRange('chr14', 105246419 + 1, 105246558 + 0)).toEqual(jasmine.any(cSortedSet));
+				expect(vcf.findGeneExonsForChrRange('chr14', 105246419 + 1, 105246558 + 0).length).toEqual(0);
+			});
+
+			/* TODO: Add GeneExons
+			it('should return a SortedSet with not a 0 length when no geneExons in Vcf object', function(){
+				expect(vcf.findGeneExonsForChrPos('chr14', 105246419 + 1, 105246558 + 0).length).not.toEqual(0);
+			});
+			*/
+		});
+
+		describe('getBedBamVcfFileUrlAsString', function(){
+			it('should be defined', function(){
+				expect(vcf.getBedBamVcfFileUrlAsString).toBeDefined();
+			});
+
+			it('should', function(){
+				//expect(vcf.getFilteredAnnotatedVariantCount()).toEqual(0);
+			});
 		});
 	});
 });
