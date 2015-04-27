04fb42e66e2e7080b9426d3a66d65c484297bc85
Remi Marenco <remi.marenco@gmail.com>
2015-04-27 10:31:16 +0200

    End of Vcf constructor

--- src/scripts/models/Vcf.js
+++ src/scripts/models/Vcf.js
@@ -4,11 +4,21 @@ var cSortedSet = require("collections/sorted-set");
 var cList = require("collections/list");
 var cSortedMap = require("collections/sorted-map");
 
+var GeneExon = require("./GeneExon.js");
+
 /**
- * [Vcf description]
+ * VCF stands for Variant Call Format, and this file format is used by the 1000 Genomes project to encode SNPs and other structural genetic variants.  The format is further described on the 1000 Genomes project Web site.  VCF calls are available at EBI / NCBI.
+ * @param {String} vcfFileUrl              Url of the vcf file
+ * @param {String} exonBedFileUrl          Url of the exon bed file
+ * @param {Array} exonBedLines            The lines of the exon bed file, in an array
+ * @param {String} ampliconBedFileUrl      Url of the amplicon bed file
+ * @param {String} variantTsvFileUrl       Url of the variant tsv file
+ * @param {Number} variantTsvFileLineCount Line count of the variant tsv file
+ * @param {String} bedBamFilesURL          Urls of the bed bam files
+ * @param {String} doNotCallFileUrl        Url of the doNotCallFile
  */
-function Vcf(vcfFileUrl, exonBedFileUrl, ampliconBedFileUrl,
-	variantTsvFileUrl, variantTsvFileLineCount, geneExonArray, doNotCallFileUrl){
+function Vcf(vcfFileUrl, exonBedFileUrl, exonBedLines, ampliconBedFileUrl,
+	variantTsvFileUrl, variantTsvFileLineCount, bedBamFilesURL, doNotCallFileUrl){
 	this.fileUrl = ''; // Was fileName in original Java program
 	this.doNotCallFileUrl = ''; // Was doNotCallFile in original Java program
 	this.exonBedFileUrl = ''; // Was exonBedFileName in original Java program
@@ -34,6 +44,16 @@ function Vcf(vcfFileUrl, exonBedFileUrl, ampliconBedFileUrl,
 	this.ampliconBedFileUrl = ampliconBedFileUrl;
 	this.variantTsvFileUrl = variantTsvFileUrl;
 	this.variantTsvFileLineCount = variantTsvFileLineCount;
+
+	this.bedBamFilesURL.forEach(function(bedBamFileURL){
+		this.bedBamVcfFileUrls.push(bedBamFileURL);
+	});
+	this.bedBamFilesURL.push(ampliconBedFileUrl);
+
+	exonBedLines.forEach(function(exonBedLine){
+		this.geneExons.add(new GeneExon(exonBedLine));
+	});
+	console.log(this.geneExons.length + " regions read from exon BED file");
 }
 
 Vcf.prototype = {
